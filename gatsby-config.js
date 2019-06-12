7412e5c3be33b5b4546055b6e7d273aff9e7ffdc
Matt Watson <mattwat@gmail.com>
2019-06-12 13:50:33 -0400

    setup plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,13 @@ module.exports = {
         path: `${__dirname}/src/data`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
