88c0499f20226f59fc578aa563d6822f71851e9b
Matt Watson <mattwat@gmail.com>
2020-07-30 10:42:37 -0400

    section service content

--- src/pages/service.js
+++ src/pages/service.js
@@ -12,68 +12,57 @@ const ServiceView = () => {
             <Title>Offices & Service</Title>
              
             <section id="offices">
-                <h3>Offices</h3>
-                <ul>
-                    { offices.map((item, i) => <li key={ `offices-${i}` }>{ item }</li> )}
-                </ul>
+                <article id="offices">
+                    <h3>Offices</h3>
+                    <ul>{ offices.map((item, i) => <li key={ `offices-${i}` }>{ item }</li> )}</ul>
+                </article>
             </section>
 
             <section id="services">
                 <h3>Service</h3>
-                <ul>
-                    { service.boards.map((item, i) => <li key={ `service-${i}` }>{ item }</li> )}
-                </ul>
 
-                <h4>Boards' Memberships</h4>
-                <ul>
-                    { service.boards.map((item, i) => <li key={ `service-${i}` }>{ item }</li> )}
-                </ul>
+                <article id="board-membership">
+                    <h4>Boards' Memberships</h4>
+                    <ul>{ service.boards.map((item, i) => <li key={ `service-${i}` }>{ item }</li> )}</ul>
+                </article>
 
-                <h4>Committees' Memberships</h4>
+                <article id="committee-membership">
+                    <h4>Committees' Memberships</h4>
+                    
+                    <h5>Current</h5>
+                    <ul>{ service.committees.current.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
 
-                <h4>Boards' Memberships</h4>
+                    <h5>Past</h5>
+                    <ul>{ service.committees.past.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
+                </article>
 
-                <h5>Current</h5>
-                <ul>
-                    { service.committees.current.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
-
-                <h5>Past</h5>
-                <ul>
-                    { service.committees.past.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
-
-                <h4>Committee and Conference Chairing, Co-Chairing</h4>
-
-                <ul>
-                    { service.chairing.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
-
-                <h4>Service to the State and Nation</h4>
-
-                <h5>Service to State Institutions</h5>
-                <ul>
-                    { service.government.state.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
-
-                <h5>Service to National Institutions</h5>
-                <ul>
-                    { service.government.nation.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
-
-                <h4>Service to the University</h4>
+                <article id="chairing">
+                    <h4>Committee and Conference Chairing, Co-Chairing</h4>
                 
-                <h5>Departmental Committees</h5>
-                <ul>
-                    { service.university.departmental.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
+                    <ul>{ service.chairing.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
+                </article>
 
-                <h5>College or University Committees</h5>
-                <ul>
-                    { service.university.universityWide.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}
-                </ul>
+                <article id="government-service">
+                    <h4>Service to the State and Nation</h4>
+
+                    <h5>Service to State Institutions</h5>
+                    <ul>{ service.government.state.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
+
+                    <h5>Service to National Institutions</h5>
+                    <ul>{ service.government.nation.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
+                </article>
+
+                <article id="university-service">
+                    <h4>Service to the University</h4>
+                    
+                    <h5>Departmental Committees</h5>
+                    <ul>{ service.university.departmental.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
+
+                    <h5>College or University Committees</h5>
+                    <ul>{ service.university.universityWide.map((item, i) => <li key={ `current-committees-${i}` }>{ item }</li> )}</ul>
+                </article>
             </section>
-            
+
         </div>
     )
 }
